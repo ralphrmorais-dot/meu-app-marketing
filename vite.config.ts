f8014d027cbadc2571a55b1aa5097cd4362a8075
ralphrmorais-dot <ralph.rmorais@gmail.com>
2026-02-10 16:52:54 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,7 @@
+export default defineConfig({
+  base: '/',  // <--- ADICIONE ESSA LINHA AQUI
+  plugins: [react()],
+
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
